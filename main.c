6914cf527b70d9173cf6a52749d00864a043a6d1
aicha_elya <aichaelyadri60@gmail.com>
2025-09-30 23:03:33 +0100

    acheter alimentation solde

--- main.c
+++ main.c
@@ -193,12 +193,60 @@ printf("Entrer prenom de :");
 scanf(" %[^\n]",client[0].prenom);
 }
 
+void affichesolde(){
+    printf("le client :%s %s",client[0].nom ,client[0].prenom);
+    printf("le solde disponible est : %.2f  .",client[0].solde);
+}
 
+
+void Alimentationsolde(){
+    float Montant ;
+    printf("entrer la valeur qui tu veux verser :");
+    scanf("%f",&Montant);
+    if(Montant>0){
+        client[0].solde +=Montant;
+        printf("Montant ajouter avec succes nouveau solde est %.2f \n.",client[0].solde);
+    }else{
+        printf("Montant invalide.\n");
+    }
+
+}
+
+
+void achater(){
+    char nom[50];
+    int trouve =0;
+    printf("entrer le nom de produit qui tu veux acheter :");
+    scanf(" %[^\n]",nom);
+    for(int i=0;i<nbproduits;i++){
+        if(strcmp(produit[i].nomP , nom)==0){
+            trouve =1;
+            if(produit[i].stock>0){
+                if(client[0].solde >produit[i].prix){
+                    client[0].solde -=produit[i].prix ;
+                    produit[i].stock --;
+                    printf("achat avec succes\n");
+
+                }else{
+                    printf("prix et superieur a votre solde\n");
+                }
+            }else{
+                printf("le stock de cette produit et zero .\n");
+            }
+            break;
+        } 
+
+        
+    }
+    if(trouve != 1){
+        printf("aucun produit avec cette nom .\n");
+    }
+}
 
 
 
 int main(){
-
+    achater();
 
 }
 
